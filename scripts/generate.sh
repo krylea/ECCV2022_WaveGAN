dcb5bec88649b82c5b0baca9ea812af3a0789168
Kira Selby <kaselby@uwaterloo.ca>
2023-09-21 15:24:19 -0400

    fid scripts

---
+++ scripts/generate.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --job-name=waveganfid
+#SBATCH --output=logs/slurm-%j.txt
+#SBATCH --open-mode=append
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=rtx6000,t4v2
+#SBATCH --cpus-per-gpu=2
+#SBATCH --mem=50GB
+#SBATCH --exclude=gpu109
+
+dataset=$1
+num=$2
+eval_backbone=${3:-'inception'}
+invert=${4:-0}
+
+argstring="--gpu 0 --dataset $dataset --num $num \
+--eval_backbone $eval_backbone \
+--name results/${dataset}_sept18 \
+--real_dir fids/${dataset}_${num}/real --ckpt ${dataset}_checkpoint.pt \
+--fake_dir fids/${dataset}_${num}/fake"
+
+if [ $invert -eq 1 ]
+then
+    argstring="${argstring} --invert_rgb"
+fi
+
+python main_metric.py $argstring
+
